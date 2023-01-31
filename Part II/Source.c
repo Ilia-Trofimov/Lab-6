70f206962eeafd4420d349b7f97f06d5b91b285d
Андрей Боев <a.boev.it@gmail.com>
2023-01-31 19:37:32 +0300

    Доработка Part II

--- Part II/Source.c
+++ Part II/Source.c
@@ -26,37 +26,69 @@ struct book
 
 typedef struct book Book;
 
-void readBook(Book* book)
-{
-	printf("Название: ");
-	// scanf чистает строку до первого пробела
-	// Чтобы считать всю строк целиком, необходимо использовать gets
-	gets_s(book->name, STR_LENGTH);
-	printf("Цена: ");
-	scanf("%f", &book->price);
-	getchar();
-	printf("Число страниц: ");
-	scanf("%d", &book->pageCount);
-	getchar();
-	printf("Автор: ");
-	gets_s(book->author, STR_LENGTH);
-	printf("Издательство:\n");
-	printf("\tГод издания: ");
-	scanf("%d", &book->publishing.year);
-	getchar();
-	printf("\tНазвание: ");
-	gets_s(book->publishing.name, STR_LENGTH);
-	printf("\tГород: ");
-	gets_s(book->publishing.city, STR_LENGTH);
-}
-
 void getBooksByPublishingYear(Book books[], int size, int year, char* city)
 {
+	// Режимы открытия файла:
+	// "r" - чтение
+	// "w" - запись (если файла нет, он создаётся)
+	// "a" - дозапись в конец файла
+	FILE* f = fopen("results.txt", "w");
 	for (int i = 0; i < size; i++) {
 		if (books[i].publishing.year > year && strcmp(books[i].publishing.city, city) == 0) {
-			printf("%s\n", books[i].name);
+			fprintf(f, "%s\n", books[i].name);
 		}
 	}
+	// Если при записи в файл не закрыть его, то ничего не запишется.
+	fclose(f);
+}
+
+void loadFromFile(Book books[], int* size, char* fileName)
+{
+	FILE* f = fopen(fileName, "r");
+	// Переменная для чтения строки из файла
+	char line[STR_LENGTH];
+	// Переменная для разделения строки на части (функция strtok)
+	char* ptr;
+	// Переменная для контроля записи в структуру. (0 - имя книги, 1 - цена, и т.д.)
+	int index = 0;
+	while (fgets(line, STR_LENGTH, f))
+	{
+		ptr = strtok(line, "|\n");
+		index = 0;
+		while (ptr)
+		{
+			switch (index)
+			{
+			case 0:
+				strcpy(books[*size].name, ptr);
+				break;
+			case 1:
+				sscanf(ptr, "%f", &books[*size].price);
+				break;
+			case 2:
+				sscanf(ptr, "%i", &books[*size].pageCount);
+				break;
+			case 3:
+				strcpy(books[*size].author, ptr);
+				break;
+			case 4:
+				sscanf(ptr, "%i", &books[*size].publishing.year);
+				break;
+			case 5:
+				strcpy(books[*size].publishing.name, ptr);
+				break;
+			case 6:
+				strcpy(books[*size].publishing.city, ptr);
+				break;
+			}
+
+			index++;
+			ptr = strtok(NULL, "|\n");
+		}
+		// После прочтения каждой строки, увеличиваем кол-во элементов в массиве
+		(*size)++;
+	}
+	fclose(f);
 }
 
 int main()
@@ -64,9 +96,17 @@ int main()
 	setlocale(0, "RU");
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-
-
-
-
+	int size = 0;
+	int year;
+	char city[STR_LENGTH];
+	Book books[MAX_COUNT];
+	loadFromFile(books, &size, "input.txt");
+	printf("Введите год: ");
+	scanf("%d", &year);
+	getchar();
+	printf("Введите город: ");
+	gets_s(city, STR_LENGTH);
+	getBooksByPublishingYear(books, size, year, city);
+	printf("Данные успешно загружены в файл results.txt\n");
 	return 0;
 }
